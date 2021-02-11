6f1c6c0068b1f49e672e65e72544a13ab2207457
timthomasdev <tim.thomas@hey.com>
2021-02-11 10:35:04 -0600

    Read holdings, API key from file

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
+use dirs::home_dir;
 use reqwest;
 use serde::Deserialize;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
 use tokio;
 
 #[derive(Deserialize, Debug)]
@@ -19,37 +22,46 @@ struct MarketData {
 }
 
 #[tokio::main]
-async fn main() -> Result<(), reqwest::Error> {
-    let mut porto_sum: f32 = 0.0;
-
-    let holdings = [
-        ("xtz", 5950.0),
-        ("ltc", 3.4102915),
-        ("eth", 0.97343448),
-        ("btc", 0.03997992),
-    ];
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let client = reqwest::Client::new();
 
-    for (asset, amount) in holdings.iter() {
-        let api_url = format!(
-            "https://data.messari.io/api/v1/assets/{}/metrics/market-data",
-            asset
-        );
-        let res = client
-            .get(&api_url)
-            .header("x-messari-api-key", "bfe242da-82ab-46f6-abdc-7037a4ad87e7")
-            .send()
-            .await?
-            .json::<Asset>()
-            .await?;
+    let mut porto_sum: f32 = 0.0;
 
-        println!(
-            "Current price (USD) of {}: {}",
-            res.data.name, res.data.market_data.price_usd
-        );
-        let porto_worth = res.data.market_data.price_usd * amount;
-        println!("worth: {}", porto_worth);
-        porto_sum += porto_worth;
+    let mut filename = home_dir().unwrap();
+    filename.push(".portfolio");
+
+    let file = File::open(filename)?;
+    let mut reader = BufReader::new(file);
+    let mut api_key = String::new();
+
+    reader.read_line(&mut api_key)?;
+
+    for line in reader.lines() {
+        if let Ok(holding) = line {
+            let mut split = holding.split_whitespace();
+            let asset = split.next().unwrap();
+            let amount = split.next().unwrap().parse::<f32>().unwrap();
+
+            let api_url = format!(
+                "https://data.messari.io/api/v1/assets/{}/metrics/market-data",
+                asset
+            );
+            let res = client
+                .get(&api_url)
+                .header("x-messari-api-key", "bfe242da-82ab-46f6-abdc-7037a4ad87e7")
+                .send()
+                .await?
+                .json::<Asset>()
+                .await?;
+
+            println!(
+                "Current price (USD) of {}: {}",
+                res.data.name, res.data.market_data.price_usd
+            );
+            let porto_worth = res.data.market_data.price_usd * amount;
+            println!("worth: {}", porto_worth);
+            porto_sum += porto_worth;
+        }
     }
 
     println!("Total portfolio: {}", porto_sum);
